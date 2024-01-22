03064c72642c50ef557da964070e4ac6becfa8b2
David Clement <david@zudu.co.uk>
2024-01-22 15:41:00 +0000

    title bar

---
+++ wp-content/themes/zudutron/Components/BlockTitleBar/functions.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Flynt\Components\BlockTitleBar;
+
+use Flynt\FieldVariables;
+
+add_filter('Flynt/addComponentData?name=BlockTitleBar', function ($data) {
+    if (isset($data['options']['sectionAnchor'])) {
+        $data['options']['sectionAnchor'] = preg_replace('/[^A-Za-z0-9]/', '-', strtolower($data['options']['sectionAnchor']));
+    }
+
+    
+
+    return $data;
+});
+
+function getACFLayout()
+{
+    return [
+        'name' => 'blockTitleBar',
+        'label' => __('Block: Title Bar', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('Content', 'flynt'),
+                'name' => 'contentTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            // FieldVariables\setIntro(),
+            [
+                'label' => __('Title', 'flynt'),
+                'name' => 'titleText',
+                'type' => 'text',
+                
+            ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\setContainerSize(),
+                    FieldVariables\setBackgroundColor(),
+                    FieldVariables\setPadding(),
+                    FieldVariables\setPaddingSize(),
+                    // FieldVariables\setBackgroundPattern(),
+                    FieldVariables\setAnchor()
+                ]
+            ]
+        ]
+    ];
+}
+
+
+// Options::addTranslatable('NavigationJs', [
+
+// ]);
+
+// Options::addGlobal('NavigationJs', [
+   
+// ]);
--- wp-content/themes/zudutron/inc/fieldGroups/pageComponents.php
+++ wp-content/themes/zudutron/inc/fieldGroups/pageComponents.php
@@ -15,7 +15,7 @@ add_action('Flynt/afterRegisterComponents', function () {
                 'type' => 'flexible_content',
                 'button_label' => __('Add Component', 'flynt'),
                 'layouts' => [
-                   
+                    Components\BlockTitleBar\getACFLayout(),
                     Components\BlockAccordions\getACFLayout(),
                     Components\BlockSplitContent\getACFLayout(),
                     Components\BlockWysiwyg\getACFLayout(),
