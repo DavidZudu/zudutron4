2e41a8a47bf15b007205d6788debc33a961a4684
David Clement <david@zudu.co.uk>
2024-08-16 10:43:59 +0100

    tax and terms fixed

--- wp-content/themes/zudutron/inc/getTaxAndTerms.php
+++ wp-content/themes/zudutron/inc/getTaxAndTerms.php
@@ -1,5 +1,20 @@
 <?php 
-
+function getTaxAndTerms($post)
+{
+    $taxs = [];
+    $taxNames = get_object_taxonomies($post);
+    if (($key = array_search('post_format', $taxNames)) !== false) {
+        unset($taxNames[$key]);
+    }
+    foreach ($taxNames as $tax) {
+        array_push($taxs, get_taxonomy($tax));
+    }
+    foreach ($taxs as $key => $value) {
+        $terms = get_the_terms($post->ID, $value->name);
+        $taxs[$key]->terms = $terms;
+    }
+    return $taxs;
+}
 
 function get_taxonomies_for_post_type($post_type, $exclude_taxonomies = array()) {
     // Initialize an array to store taxonomies information
